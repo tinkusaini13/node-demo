b3ad40cd48472c9362029f70e1876c81e9b2ee94
root <root@ip-172-31-40-12.ap-south-1.compute.internal>
2024-04-28 10:26:18 +0000

    node running app

---
+++ node2/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const path = __dirname + '/views/';
+const port = 3001;
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+router.get('/bmc', function(req,res){
+  res.sendFile(path + 'bmc.html');
+});
+app.use(express.static(path));
+app.use('/', router);
+app.listen(port, function () {
+  console.log('Listening on port 3001')
+})
